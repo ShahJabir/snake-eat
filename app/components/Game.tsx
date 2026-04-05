1465532e03d54747b44c4439efee70725d69c490
ShahJabir <shahjabirofficial@gmail.com>
2026-04-06 01:08:16 +0600

    feat: enhance gesture handling with directional logging

--- app/components/Game.tsx
+++ app/components/Game.tsx
@@ -6,7 +6,25 @@ import { Colors } from "../styles/colors";
 
 export default function Game() {
   const handleGesture = (event: any) => {
-    console.log(event);
+    const { translationX, translationY } = event.nativeEvent;
+    console.log(`(x:${translationX}, y:${translationY})`);
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      if (translationX > 0) {
+        // Handle right swipe
+        console.log("Moving Right");
+      } else {
+        // Handle left swipe
+        console.log("Moving Left");
+      }
+    } else {
+      if (translationY > 0) {
+        // Handle down swipe
+        console.log("Moving Down");
+      } else {
+        // Handle up swipe
+        console.log("Moving Up");
+      }
+    }
   };
   return (
     <PanGestureHandler onGestureEvent={handleGesture}>
