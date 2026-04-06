50a17cec1da3a773e1f81d1213561ab580659aa5
ShahJabir <shahjabirofficial@gmail.com>
2026-04-06 20:58:07 +0600

    feat: implement static Snake component for rendering the snake in the game

--- app/components/Game.tsx
+++ app/components/Game.tsx
@@ -1,9 +1,10 @@
-import * as React from "react";
-import { StyleSheet } from "react-native";
+import { JSX, useState } from "react";
+import { StyleSheet, View } from "react-native";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "../styles/colors";
-import { GestureEventType } from "../types/types";
+import { Coordinate, Direction, GestureEventType } from "../types/types";
+import Snake from "./Snake";
 
 const SNAAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
 const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
@@ -11,32 +12,34 @@ const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
 const MOVE_INTERVAL = 50;
 const SCORE_INCREAMENT = 10;
 
-export default function Game(): React.JSX.Element {
+export default function Game(): JSX.Element {
+  const [direction, setDirection] = useState<Direction>(Direction.Right);
+  const [snake, setSnake] = useState<Coordinate[]>(SNAAKE_INITIAL_POSITION);
+  const [food, setFood] = useState<Coordinate>(FOOD_INITIAL_POSITION);
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
+  const [isPaused, setIsPaused] = useState<boolean>(false);
   const handleGesture = (event: GestureEventType) => {
     const { translationX, translationY } = event.nativeEvent;
-    console.log(`(x:${translationX}, y:${translationY})`);
     if (Math.abs(translationX) > Math.abs(translationY)) {
       if (translationX > 0) {
-        // Handle right swipe
-        console.log("Moving Right");
+        setDirection(Direction.Right);
       } else {
-        // Handle left swipe
-        console.log("Moving Left");
+        setDirection(Direction.Left);
       }
     } else {
       if (translationY > 0) {
-        // Handle down swipe
-        console.log("Moving Down");
+        setDirection(Direction.Down);
       } else {
-        // Handle up swipe
-        console.log("Moving Up");
+        setDirection(Direction.Up);
       }
     }
   };
   return (
     <PanGestureHandler onGestureEvent={handleGesture}>
       <SafeAreaView style={styles.container}>
-        {/* Game content goes here */}
+        <View style={styles.boundaries}>
+          <Snake snake={snake} />
+        </View>
       </SafeAreaView>
     </PanGestureHandler>
   );
@@ -46,5 +49,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.primary,
+  },
+  boundaries: {
+    flex: 1,
+    borderColor: Colors.primary,
+    borderWidth: 12,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    backgroundColor: Colors.background,
   },
 });
