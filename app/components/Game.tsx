e1c735f42d7362d20aaa465af0312278c493dc8a
ShahJabir <shahjabirofficial@gmail.com>
2026-04-06 11:49:37 +0600

    feat: add gesture event type definitions and initial veriable in Game component

--- app/components/Game.tsx
+++ app/components/Game.tsx
@@ -3,9 +3,16 @@ import { StyleSheet } from "react-native";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "../styles/colors";
+import { GestureEventType } from "../types/types";
 
-export default function Game() {
-  const handleGesture = (event: any) => {
+const SNAAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREAMENT = 10;
+
+export default function Game(): React.JSX.Element {
+  const handleGesture = (event: GestureEventType) => {
     const { translationX, translationY } = event.nativeEvent;
     console.log(`(x:${translationX}, y:${translationY})`);
     if (Math.abs(translationX) > Math.abs(translationY)) {
